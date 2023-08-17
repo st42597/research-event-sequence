73192c9544bd8551829da43bebcf9f9eb3f9a943
SeongHyeon Cho <67233988+st42597@users.noreply.github.com>
2023-08-17 15:03:21 +0900

    fix: fix header name

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <header className="flex items-center justify-center border-b p-2 text-2xl">
-      Event Sequence
+      Event Sequence Laboratory
     </header>
   );
 }
