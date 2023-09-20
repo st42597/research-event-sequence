1d569b0127029e71c301d5b8aa5a3a45b7838a05
st42597 <st42597@gmail.com>
2023-09-20 12:06:37 +0900

    feat: add WS key move function(that have many errors)

--- client/src/pages/sentence/index.tsx
+++ client/src/pages/sentence/index.tsx
@@ -4,18 +4,18 @@ import * as d3 from "d3";
 export default function SentenceVis() {
   const [label, setLabel] = useState([]);
   const [selectedRow, setSelectedRow] = useState(null);
+  const [rowOrder, setRowOrder] = useState([...Array(1000).keys()]);
   const canvas = useRef(null);
 
   const color = d3.schemeTableau10;
   let cnt = 0;
   const event2color: any = {};
-  const row2idx: any = {};
-  const rowOrder = [];
 
   useEffect(() => {
     let _label: any = [];
+    const row2idx: any = {};
     const loadData = async () => {
-      const data: any = await d3.json("/iraqwar.json");
+      let data: any = await d3.json("/iraqwar.json");
       const numCol = Math.max(...data.map((x: Array<any>) => x.length));
       const numRow = data.length;
       const rectWidth = 100;
@@ -25,7 +25,7 @@ export default function SentenceVis() {
 
       for (let i = 0; i < data.length; i++) {
         for (let j = 0; j < data[i].length; j++) {
-          if (data[i][j][1][0] != "#") {
+          if (data[i][j][1][0] !== "#") {
             event2color[data[i][j][1]] = "lightgray";
           }
           if (!(data[i][j][1] in event2color)) {
@@ -37,7 +37,7 @@ export default function SentenceVis() {
       }
 
       function countKeyword(a: any) {
-        return a.filter((d: any) => d[1][0] == "#").length;
+        return a.filter((d: any) => d[1][0] === "#").length;
       }
 
       data.sort((a: any, b: any) => {
@@ -45,9 +45,14 @@ export default function SentenceVis() {
       });
 
       for (let i = 0; i < data.length; i++) {
-        row2idx[data[i]] = i;
-        rowOrder.push(i);
+        row2idx[data[i]] = rowOrder[i];
       }
+      let _data = [...data];
+      for (let i = 0; i < data.length; i++) {
+        _data[rowOrder[i]] = data[i];
+      }
+      data = _data;
+      console.log(rowOrder);
 
       const xScale = d3
         .scaleBand()
@@ -59,19 +64,25 @@ export default function SentenceVis() {
         .domain([...Array(numRow).keys()]);
 
       let svg: any;
+      let rectG: any;
+      let selectG: any;
       if (d3.select(canvas.current).select("svg").empty()) {
         svg = d3
           .select(canvas.current)
           .append("svg")
           .attr("width", width)
           .attr("height", height);
+        rectG = svg.append("g").attr("id", "rectG");
+        selectG = svg.append("g").attr("id", "selectG");
       } else {
         svg = d3.select(canvas.current).select("svg");
+        rectG = svg.select("#rectG");
+        selectG = svg.select("#selectG");
       }
 
-      svg
+      rectG
         .selectAll("g")
-        .data(data)
+        .data(data, (d) => d)
         .join(
           (enter) =>
             enter
@@ -86,7 +97,7 @@ export default function SentenceVis() {
               (d: any, i: number) => `translate(0, ${yScale(i)!})`,
             ),
         )
-        .selectAll("g")
+        .selectAll("rect")
         .data((d) => d)
         .join((enter) =>
           enter
@@ -96,9 +107,9 @@ export default function SentenceVis() {
             .attr("x", (d: any, i: number) => xScale(i))
             .attr("fill", (d: any, i: number) => event2color[d[1]]),
         );
-      svg
-        .selectAll()
-        .data(data, (d, i) => i)
+      selectG
+        .selectAll("rect")
+        .data(data, (d) => d)
         .join(
           (enter) =>
             enter
@@ -110,41 +121,90 @@ export default function SentenceVis() {
               .attr("width", width)
               .attr("height", rectHeight)
               .attr("fill", (d: any) => {
-                if (row2idx[d] == selectedRow) return "red";
                 return "transparent";
               })
               .attr("opacity", 0.2)
               .on("mouseover", function (_, d) {
-                if (row2idx[d] == selectedRow) return;
                 d3.select(this).attr("fill", "black");
               })
               .on("mouseout", function (_, d) {
-                if (row2idx[d] == selectedRow) return;
                 d3.select(this).attr("fill", "transparent");
               })
               .on("click", function (_, d) {
-                console.log(row2idx[d]);
                 setSelectedRow(row2idx[d]);
+              })
+              .on("keydown", function (e, d) {
+                const src = row2idx[d];
+                let target: number;
+                if (e.keyCode === 87) {
+                  target = Math.max(0, src - 1);
+                } else if (e.keyCode === 83) {
+                  target = Math.min(numRow - 1, src + 1);
+                }
+                let _rowOrder = [...rowOrder];
+                let srcIdx = 0;
+                let targetIdx = 0;
+                for (let i = 0; i < data.length; i++) {
+                  if (rowOrder[i] === src) srcIdx = i;
+                  if (rowOrder[i] === target) targetIdx = i;
+                }
+                _rowOrder[srcIdx] = target;
+                _rowOrder[targetIdx] = src;
+                setRowOrder(_rowOrder);
+                setSelectedRow(target);
+              })
+              .on("focus", function () {
+                d3.select(this).attr("fill", "red").style("outline", "none");
               }),
           (update) =>
-            update.attr("fill", (d: any) => {
-              console.log("test");
-              if (row2idx[d] == selectedRow) return "red";
-              return "transparent";
-            }),
+            update
+              .attr("fill", (d: any) => {
+                console.log(row2idx[d], d.length, selectedRow);
+                if (row2idx[d] === selectedRow) return "red";
+                return "transparent";
+              })
+              .on("mouseover", function (_, d) {
+                if (row2idx[d] === selectedRow) return;
+                d3.select(this).attr("fill", "black");
+              })
+              .on("mouseout", function (_, d) {
+                if (row2idx[d] === selectedRow) return;
+                d3.select(this).attr("fill", "transparent");
+              })
+              .on("keydown", function (e, d) {
+                const src = row2idx[d];
+                let target: number;
+                if (e.keyCode === 87) {
+                  target = Math.max(0, src - 1);
+                } else if (e.keyCode === 83) {
+                  target = Math.min(numRow - 1, src + 1);
+                } else return;
+                let _rowOrder = [...rowOrder];
+                let srcIdx = 0;
+                let targetIdx = 0;
+                for (let i = 0; i < data.length; i++) {
+                  if (rowOrder[i] === src) srcIdx = i;
+                  if (rowOrder[i] === target) targetIdx = i;
+                }
+                _rowOrder[srcIdx] = target;
+                _rowOrder[targetIdx] = src;
+                setRowOrder(_rowOrder);
+                setSelectedRow(target);
+              })
+              .on("focus", function () {
+                d3.select(this).attr("fill", "red").style("outline", "none");
+              }),
         );
     };
 
     loadData().then(() => {
       setLabel(_label);
     });
-  }, [selectedRow]);
-
-  useEffect(() => {}, [label]);
+  }, [selectedRow, rowOrder]);
 
   return (
     <>
-      <div ref={canvas}></div>
+      <div className="overflow" ref={canvas}></div>
       <div className="fixed bottom-0 right-0 h-80 w-60 bg-slate-300 p-4">
         <ul>
           {label.map((x, i) => {
